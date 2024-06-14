08de3556dc24a9d3c456a9a294b0b97df032e22b
Pavel Danilov <danilov211@mail.ru>
2024-06-14 16:59:19 +0300

    "add homework 11"

--- src/main/java/ru/gb/hw6/controllers/NoteController.java
+++ src/main/java/ru/gb/hw6/controllers/NoteController.java
@@ -1,7 +1,10 @@
 package ru.gb.hw6.controllers;
 
-import  ru.gb.hw6.model.Note;
-import  ru.gb.hw6.services.NoteService;
+import ru.gb.hw6.model.Note;
+import ru.gb.hw6.model.exceptions.ResourceNotFoundException;
+import ru.gb.hw6.services.NoteService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -9,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 /**
  * Класс NoteController обрабатывает http запросы
@@ -20,23 +24,65 @@ import java.util.List;
 public class NoteController {
 
     /**
-     * Поле взаимодействия с базой данных
+     * Метрика количество запросов
+     */
+    private final Counter requestCounter = Metrics.counter("request_count");
+
+    /**
+     * Сервис обработки заметок
      */
     private NoteService noteService;
 
     /**
-     * Метод просмотра всех заметок
-     * @return список всех заметок
+     * Метод получения всех заметок
+     * @return ответ со списком всех заметок в бд
      */
     @GetMapping
     public ResponseEntity<List<Note>> getAllNotes() {
         log.info("Getting all available notes");
-        return ResponseEntity.ok(noteService.getAllNotes());
+        requestCounter.increment();
+        return ResponseEntity.ok().body(noteService.getAllNotes());
     }
 
+    /**
+     * Метод добавления заметки в бд
+     * @param note создаваемая заметка
+     * @return ответ с сохраненной в бд заметкой
+     */
     @PostMapping
     public ResponseEntity<Note> addNote(@RequestBody Note note) {
         log.info("Adding note to the data base 'notes'");
+        requestCounter.increment();
         return new ResponseEntity<>(noteService.addNote(note), HttpStatus.CREATED);
+    }
+
+    /**
+     * Метод получения заметки по идентификатору
+     * @param id получаемой заметки
+     * @return ответ с заметкой
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable long id) {
+        log.info("Getting note by id in the data base 'notes'");
+        requestCounter.increment();
+        return ResponseEntity.ok().body(noteService.findNoteById(id));
+    }
+
+    /**
+     * Метод удаления заметки по идентификатору
+     * @param id удаляемой заметки
+     * @return ответ об удалении заметки
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteNoteById(@PathVariable long id) {
+        log.info("Deleting not by id from the data base 'notes'");
+        requestCounter.increment();
+        try {
+            noteService.deleteNote(id);
+            return ResponseEntity.ok().build();
+        } catch (ResourceNotFoundException e) {
+            log.info("Note hasn't found!");
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/ru/gb/hw6/services/NoteService.java
+++ src/main/java/ru/gb/hw6/services/NoteService.java
@@ -1,6 +1,7 @@
 package ru.gb.hw6.services;
 
 import ru.gb.hw6.model.Note;
+import ru.gb.hw6.model.exceptions.ResourceNotFoundException;
 import ru.gb.hw6.repositories.NoteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,33 +9,52 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-/**
- * Класс NoteService взаимодействия с базой данных
- */
+
 @Service
 @AllArgsConstructor
 public class NoteService {
 
     /**
-     * Поле взаимодействия с базой данных
+     * Репозиторий для работы с базой данных заметок
      */
     private NoteRepository noteRepository;
 
     /**
-     * Метод просмотра всех заметок
-     * @return список всех заметок
+     * Метод получения всех заметок
+     * @return список всех заметок в бд
      */
     public List<Note> getAllNotes() {
         return noteRepository.findAll();
     }
 
     /**
-     * Метод создания заметки
+     * Метод добавления заметки в бд
      * @param note создаваемая заметка
      * @return созданная заметка
      */
     public Note addNote(Note note) {
         note.setCreationDate(LocalDate.now());
         return noteRepository.save(note);
+    }
+
+    /**
+     * Метод получения заметки по идентификатору
+     * @param id искомой заметки
+     * @return объект заметки или генерация исключения
+     */
+    public Note findNoteById(long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Note " + id + " not found!"));
+    }
+
+    /**
+     * Метод удаления заметки по идентификатору
+     * @param id заметки
+     * @throws ResourceNotFoundException исключение при отсутствии заметки в бд
+     */
+    public void deleteNote(long id) throws ResourceNotFoundException {
+        findNoteById(id);
+        noteRepository.deleteById(id);
     }
 }
